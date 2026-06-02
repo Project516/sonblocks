c36a49b64827648a6b659e64446a4ea90e311dbe
project516 <138796702+Project516@users.noreply.github.com>
2026-06-02 09:42:05 -0500

    Initial commit

---
+++ src/main/java/dev/project516/playerevolutions/util/RegistryHandler.java
@@ -0,0 +1,26 @@
+package dev.project516.playerevolutions.util;
+
+import dev.project516.playerevolutions.PlayerEvolutions;
+import dev.project516.playerevolutions.blocks.BlockItemBase;
+import dev.project516.playerevolutions.blocks.RubyBlock;
+import dev.project516.playerevolutions.items.ItemBase;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class RegistryHandler {
+    public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, PlayerEvolutions.MOD_ID);
+    public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, PlayerEvolutions.MOD_ID);
+
+    public static void init() {
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+    public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", ItemBase::new);
+    public static final RegistryObject<Block> RUBY_BLOCK = BLOCKS.register("ruby_block", RubyBlock::new);
+    public static final RegistryObject<Item> RUBY_BLOCK_ITEM = ITEMS.register("ruby_block", () -> new BlockItemBase(RUBY_BLOCK.get()));
+}
